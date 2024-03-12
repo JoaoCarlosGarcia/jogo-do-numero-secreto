4839cbcf185f49998e239ac48e391edadb1cc638
Joao Carlos Garcia <161158622+Joao-Carlos-Garcia-DEV@users.noreply.github.com>
2024-03-12 19:59:42 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('h1', 'Jogo do número Secretão');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
@@ -20,7 +20,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
      
     if (chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('h1', 'Acertoooou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
